b8c7311bbaf3a122a42d63527f9587d8e48091de
Will Cullen <will.tw.cullen@gmail.com>
2015-05-25 12:07:51 -0400

    remove cached to fix ignore

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/home/will/pebble-dev/PebbleSDK-3.0-beta12/Pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = [u'basalt', u'aplite']
